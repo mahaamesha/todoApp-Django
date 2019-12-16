8c845fa4ee497474068936c8d8db4d18d6a8ee17
mahaamesha <mahaamesha@gmail.com>
2019-12-16 08:47:52 +0700

    Duit! yes

--- todoList/views.py
+++ todoList/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from todoList.models import Todo
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
-    context = {}
-    return render(request, 'main/home.html', context)
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, 'main/home.html', {
+        "todo_items": todo_items
+    })
+
+@csrf_exempt
+def add_todo(request):
+    print(request.POST)
+    current_date = timezone.now()
+    content = request.POST["content"]
+    created_obj = Todo.objects.create(added_date=current_date, text=content)
+    #print(created_obj)
+    #print(created_obj.id)
+    length_of_todos = Todo.objects.all().count()
+    #print(length_of_todos)
+    return HttpResponseRedirect("/")
